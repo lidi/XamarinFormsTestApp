64372d239715f65dd3ee03edea00dac9971c16c3
Liutauras Diu <liutauras.diu@xitaso.com>
2016-04-15 16:59:58 +0200

    Add HelloPage

--- HelloApp/HelloApp/App.cs
+++ HelloApp/HelloApp/App.cs
@@ -12,19 +12,7 @@ namespace HelloApp
         public App()
         {
             // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new HelloPage();
         }
 
         protected override void OnStart()
---
+++ HelloApp/HelloApp/HelloPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace HelloApp
+{
+    public class HelloPage : ContentPage
+    {
+        public HelloPage()
+        {
+            this.Content = new Label { Text = "Hello Xamarin Forms!"};
+
+            Padding = new Thickness(0,20,0,20);
+        }
+    }
+}
